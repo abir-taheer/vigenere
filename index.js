4c8b506ae978bf22e9e4475a90cad4688ff4ae87
Abir Taheer <abir@taheer.me>
2020-10-19 13:47:39 -0400

    removed extra info from output

--- index.js
+++ index.js
@@ -5,29 +5,28 @@ const normalize = require("./normalize");
 const calcKeyLen = require("./calcKeyLen");
 const crackKey = require("./crackKey");
 
-console.log('-----------------------------');
 
 if (args[0] === "encode") {
 	const str = normalize(args[1]);
 	const key = normalize(args[2]);
-	console.log("encoded: ", encode(str, key));
+	const encoded = encode(str, key);
+
+	console.log(encoded);
 }
 
 if (args[0] === "decode") {
 	const str = normalize(args[1]);
 	if (args[2]) {
 		const key = normalize(args[2]);
-		console.log("decoded: ", decode(str, key));
+		const decoded = decode(str, key);
+		console.log(decoded);
 	} else {
-		console.log("Key not provided, attempting to calculate key");
 
 		const keyLen = calcKeyLen(str);
-		console.log("Assuming key is len ", keyLen);
 
 		const key = crackKey(str, keyLen);
-		console.log("Assuming key is ", key);
 
 		const decoded = decode(str, key);
-		console.log("Decoded string: ", decoded);
+		console.log(decoded);
 	}
 }
